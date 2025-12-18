97d9f05523b1e76fd814e1f68bfd438443f55cec
Jeison Gomez <gomezres011@gmail.com>
2025-12-18 10:15:01 -0500

    Implementación de las rutas y vistas de las mismas

--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -1,8 +1,26 @@
 import { createBrowserRouter } from "react-router";
+import { MediaLayout } from "../media/layouts/MediaLayout";
+import { MoviesPage } from "../media/pages/movies/MoviesPage";
+import { SeriesPage } from "../media/pages/series/SeriesPage";
+import { HomePage } from "../media/pages/home/HomePage";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hola Mundo</div>,
+    element: <MediaLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/movies",
+        element: <MoviesPage />,
+      },
+      {
+        path: "/series",
+        element: <SeriesPage />,
+      },
+    ],
   },
 ]);
